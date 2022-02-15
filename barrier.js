5320ab9d66214dfda87dbdd9f83fc7b45e0806b9
Joel Maus <jmaus06@yahoo.com>
2022-02-15 10:32:25 -0500

    added barrier.js file to control barrier objects

---
+++ barrier.js
@@ -0,0 +1,8 @@
+// Barrier class to draw walls, and barriers on the screen for player to avoid or jump over
+// It will allow to move() and draw() and have collision() detection
+export class Barrier{
+    constructor(){
+
+    }
+
+};
