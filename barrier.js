764be44b3b3da7e182364e3603d10b38c6af8ee6
Joel Maus <jmaus06@yahoo.com>
2022-02-16 11:31:12 -0500

    Added getter and setter to Barrier class

--- barrier.js
+++ barrier.js
@@ -2,7 +2,13 @@
 // It will allow to move() and draw() and have collision() detection
 export class Barrier{
     constructor(){
-
+        this.x = 0; //initial values 0 for coordinates
+        this.y = 0;
     }
+    get x() { return this.x; } //getter
+    get y() { return this.y; }
+
+    set x(xVal){ this.x = xVal; } //setter
+    set y(yVal){ this.y = yVal; }
 
 };
