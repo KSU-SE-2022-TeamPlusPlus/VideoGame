39956987650cb967b5adfe7e1dc27ed581b6de0a
Belle <70984569+cakhigbe@users.noreply.github.com>
2022-02-26 15:38:33 -0500

    Update sketch.js

--- source/sketch.js
+++ source/sketch.js
@@ -72,8 +72,8 @@ window.setup = function () {
 	runner = new Runner();
 	
 	// Make two barriers
-	objWall = new Barrier("wall", createVector(900, 240));
-	objChair = new Barrier("chair", createVector(1200, 240));
+	objWall = new Barrier("brickwall", createVector(900, 240));
+	objChair = new Barrier("lawnchair", createVector(1200, 240));
 }
 
 // This isn't necessarily required, but it does help separate state changes
