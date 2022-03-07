63959ae5b1c771320afbce4762a7fe31754636b3
vwheatle <vwheatle@kent.edu>
2022-03-07 17:09:10 -0500

    tractor

--- source/runner.js
+++ source/runner.js
@@ -1,24 +1,31 @@
 import { Timer } from "./timer.js";
 
-const RUNNER_HOME = new p5.Vector(15, 225);
+const RUNNER_HOME = new p5.Vector(60, 245);
 
 export class Runner {
 	static image;
 	
 	static preload() {
-		Runner.image = loadImage("assets/dog_runner.gif");
+		Runner.image = loadImage("assets/tractor.png");
 	}
 	
 	constructor() {
-		this.frame = 0;
+		this.time = 0;
+		this.yOffset = 0;
 		this.walkTimer = new Timer(1/12);
 	}
 	
 	update(dt) {
-		// Nothing here yet.
+		this.time += dt;
+		this.yOffset = Math.round(Math.abs(Math.sin(this.time * TAU * 16)) * 2);
 	}
 	
 	draw() {
-		image(Runner.image, RUNNER_HOME.x, RUNNER_HOME.y, 130, 130);
+		image(
+			Runner.image,
+			RUNNER_HOME.x - 250 / 2,
+			RUNNER_HOME.y - 250 / 2 + this.yOffset,
+			250, 250
+		);
 	}
 }
