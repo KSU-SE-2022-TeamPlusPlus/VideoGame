33a74c9ce36524f8b32cbe60678f683c70da41f7
Joel Maus <jmaus06@yahoo.com>
2022-04-05 13:46:05 -0400

    barrier tests added

---
+++ tests/testBarriers.js
@@ -0,0 +1,17 @@
+import { Barrier } from "../source/barrier.js";
+import { BarrierManager } from "../source/barrierManager.js";
+
+const TIME_AMOUNT = 1/60;
+
+testGroup("barrier",
+	test("'loaded' event fires", function() {
+	
+		
+		assert(true);
+	}),
+	test("'drawn correctly' event fires", function() {
+	
+		
+		assert(false);
+	})
+);
