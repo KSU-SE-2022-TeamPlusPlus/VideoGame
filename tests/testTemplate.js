1540ddf2b88e01ce701ba05b7bab849134d88e34
vwheatle <vwheatle@kent.edu>
2022-04-08 21:22:26 -0400

    fix link and changes to test page

---
+++ tests/testTemplate.js
@@ -0,0 +1,13 @@
+import { Thing } from "../source/thing.js";
+
+testGroup("name of the thing you're testing",
+	test("test name 1", function() {
+		// do something...
+	}),
+	test("test name 2", function() {
+		// do something else...
+	}),
+	test("test name 3", function() {
+		// do a third thing...
+	}),
+);
